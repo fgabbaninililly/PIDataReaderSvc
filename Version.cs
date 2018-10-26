8f709f4e3400884978fe7064e845ff54bdda88b7
fgabbaninililly <gabbanini_francesco@lilly.com>
2018-10-26 08:58:56 +0200

    Bump version to 1.2.1

--- Version.cs
+++ Version.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace PIDataReaderSvc {
 	public class Version {
-		public static readonly string version = "1.2.0";
+		public static readonly string version = "1.2.1";
 
 		public static string getVersion() {
 			return string.Format("PIDataReader Service Launcher v{0}", version);
